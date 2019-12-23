3d61fb6c65f32c4acb19e7385372c942f4785ffc
johnmuthurimi <mucunga90@gmail.com>
2019-12-23 12:30:27 +0300

    added rabbit mq

---
+++ email-service/src/main/java/slick/mali/email/EmailController.java
@@ -0,0 +1,31 @@
+package slick.mali.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping
+    public String call() {
+        return "I'm email running on port " + environment.getProperty("local.server.port");
+    }
+
+    @GetMapping("/slow")
+    public String slow() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+
+        }
+        return "I'm Slow email running on port " + environment.getProperty("local.server.port");
+    }
+
+}
