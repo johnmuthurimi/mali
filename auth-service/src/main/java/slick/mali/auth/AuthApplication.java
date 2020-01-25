61435b71a77ce8239019b5e85f5abe72a16f6634
John Peter <mucunga90@gmail.com>
2020-01-25 07:30:00 +0300

    merge

---
+++ auth-service/src/main/java/slick/mali/auth/AuthApplication.java
@@ -0,0 +1,33 @@
+package slick.mali.auth;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class AuthApplication implements CommandLineRunner {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	RestTemplate template() {
+		return new RestTemplate();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		rabbitTemplate.convertAndSend("TestExchange", "testRouting", "Hello from code");
+	}
+
+}
---
+++ auth-service/src/main/java/slick/mali/auth/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,78 @@
+package slick.mali.auth.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    /**
+     * RabbitMQ host
+     */
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    /**
+     * RabbitMQ port
+     */
+    @Value("${spring.rabbitmq.port}")
+    private String port;
+
+    /**
+     * RabbitMQ username
+     */
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    /**
+     * RabbitMQ password
+     */
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    /**
+     * 
+     * Login queue name
+     */
+    private static final String LOGIN_QUEUE = "mali.auth-service.login-queue";
+
+    /**
+     * Create login queue
+     * @return Queue
+     */
+    @Bean
+    Queue loginQueue() {
+        return new Queue(LOGIN_QUEUE, true);
+    }
+
+    /**
+     * Persist the RabbitMQ connection thru caching
+     * @return ConnectionFactory
+     */
+    @Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+        return cachingConnectionFactory;
+    }
+
+    /**
+     * Create a message listener container
+     * @return simpleMessageListenerContainer
+     */
+    @Bean
+    MessageListenerContainer messageListenerContainer() {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+        simpleMessageListenerContainer.setQueues(loginQueue());
+        simpleMessageListenerContainer.setMessageListener(new LoginQueueMessageListener());
+        return simpleMessageListenerContainer;
+    }
+}
