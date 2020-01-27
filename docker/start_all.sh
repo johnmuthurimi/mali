f9c60684eb0d6ba2fb0a8bd3ec8e46be744265eb
johnmuthurimi <mucunga90@gmail.com>
2020-01-27 17:13:04 +0300

    Included consul custer

--- docker/start_all.sh
+++ docker/start_all.sh
@@ -3,10 +3,14 @@
 # get the absolute path of the executable
 SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH="$SELF_PATH"/$(basename -- "$0")
 
-echo "Stopping all"
+echo "Stopping all services"
 docker ps | \
-grep "consul-service\|rabbitmq-service\|gateway-service\|auth-service\|email-service" | \
+grep "consul-service-node1\|consul-service-node2\|consul-service-node3\|rabbitmq-service\|gateway-service" | \
 awk '{print $1}' | xargs docker stop
+
+# remove all images, containers, volumes and networks
+echo "Pruning all the services"
+docker system prune
 
 echo -n "Build new images? y/n  "
 read build_images
@@ -14,23 +18,13 @@ if [ "$build_images" == "y" ]; then
 	echo "Generating gateway-service image..."
 	cd ./../gateway-service/
 	sh ./build_image.sh
-	
-	echo "Generating auth-service image..."
-	cd ./../auth-service/
-	sh ./build_image.sh
-	
-	echo "Generating email-service image..."
-	cd ./../email-service/
-	sh ./build_image.sh
+
 fi
 
 if [ "$build_images" == "y" ]; then
 	echo -n "Docker credentials for publishing images"
 	#docker login
-	docker images
 	#docker push mucunga90/gateway-service:latest
-	#docker push mucunga90/auth-service:latest
-	#docker push mucunga90/email-service:latest
 fi
 
 #echo "Starting your local dockerized full stack with mounted volumes"
