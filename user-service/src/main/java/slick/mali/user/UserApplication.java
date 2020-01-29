51d4cc1fb2360e1387939ca85df7cef5e4e7c9e5
johnmuthurimi <mucunga90@gmail.com>
2020-01-29 08:25:29 +0300

    Rabbit MQ was causign crashes in user service Now all service are running

--- user-service/src/main/java/slick/mali/user/UserApplication.java
+++ user-service/src/main/java/slick/mali/user/UserApplication.java
@@ -1,22 +1,11 @@
 package slick.mali.user;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class UserApplication implements CommandLineRunner {
-
-	/**
-	 * Declare rabbit MQ template
-	 */
-	@Autowired
-	private RabbitTemplate rabbitTemplate;
+public class UserApplication {
 
 	/**
 	 * Main function with arguments
@@ -24,16 +13,4 @@ public class UserApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 	}
-
-	@LoadBalanced
-	@Bean
-	RestTemplate template() {
-		return new RestTemplate();
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		rabbitTemplate.convertAndSend("TestExchange", "testRouting", "Hello from code");
-	}
-
 }
