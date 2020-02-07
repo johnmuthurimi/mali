7b7d12ea0a7260eedf179d02e2249fb631638e75
johnmuthurimi <mucunga90@gmail.com>
2020-02-07 18:51:17 +0300

    Add existing file

---
+++ user-service/src/main/resources/db/migration/V1.2.0__sp_userfetch.sql
@@ -0,0 +1,18 @@
+USE user_service;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS sp_user_fetch;
+//
+
+DELIMITER //
+CREATE PROCEDURE sp_user_fetch(pageNumber int)
+BEGIN
+  DECLARE records INT DEFAULT 30;
+  DECLARE offset INT DEFAULT 1;
+
+  SET @offset = (pageNumber*records);
+
+  SELECT * FROM user LIMIT offset, records;
+
+END //
+DELIMITER ;
