728c47711355bc7416d5ec4f0384e95abe7f29d2
john peter <mucunga90@gmail.com>
2020-04-05 00:36:25 +0300

    Docker Swarm
    Improved the deployment to use docker swarm in a single host

--- docker/start_all.sh
+++ docker/start_all.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-echo "See Memory and CPU Usage for All Your Docker Containers"
-docker ps -q | xargs  docker stats --no-stream
-
 # get the absolute path of the executable
 SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH="$SELF_PATH"/$(basename -- "$0")
 
-# remove all images, containers, volumes and networks
-echo "Pruning all the services"
-docker system prune -a --volumes
+echo "Take down the stack (application)"
+docker stack rm dev
 
-echo "Stopping all services"
-docker ps | \
-grep "consul-service\|rabbitmq-service\|mysql-service\|gateway-service\|user-service\|alert-service\|web-service" | \
-awk '{print $1}' | xargs docker stop
+echo "Take down the Swarm cluster"
+docker swarm leave --force
+
+echo "System prune all images, containers and volumes"
+docker system prune -a
 
 echo -n "Build new images? y/n  "
 read build_images
@@ -60,25 +57,15 @@ fi
 echo "Starting your local dockerized full stack with mounted volumes"
 cd ./../docker/
 
+echo "Create Swarm Cluster"
+docker swarm init
+
 # update which environment
 echo -n "Build which environment: [dev/prod]  "
 read build_env
 
-echo -n "Compose configuration services: [y/n]  "
-read build_config
-if [ "$build_config" == "y" ]; then
-	docker-compose -f $build_env/docker-compose-config.yml up -d
-fi
+echo "Deploy application services"
+docker stack deploy -c $build_env/docker-compose.yml $build_env
 
-echo -n "Compose application services: [y/n]  "
-read build_service
-if [ "$build_service" == "y" ]; then
-	docker-compose -f $build_env/docker-compose-service.yml up -d
-fi
-
-echo "See Memory and CPU Usage for All Your Docker Containers"
-docker ps -q | xargs  docker stats --no-stream
-
-
-
-
+echo "Check the Status of the Running Services on a Swarm Cluster"
+docker service ls
