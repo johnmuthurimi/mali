710b2dd2483cd501a6e9b40b24edf3860bad2157
john peter <mucunga90@gmail.com>
2020-04-13 22:36:22 +0300

    Portainer is a great deal

--- docker/stop_all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# get the absolute path of the executable
-SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH="$SELF_PATH"/$(basename -- "$0")
-
-echo "Stopping all"
-docker ps | \
-grep "consul-service\|rabbitmq-service\|mysql-service\|gateway-service\|user-service\|alert-service\|web-service" | \
-awk '{print $1}' | xargs docker stop
