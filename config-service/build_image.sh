9a6aced259aa2e1eb3f197b0854d64dbaacad4cf
john peter <mucunga90@gmail.com>
2020-04-28 16:56:20 +0300

    fixed issues on including all services to metrics

--- config-service/build_image.sh
+++ config-service/build_image.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -rf target/*.jar
 mvn clean package spring-boot:repackage
 
-docker image build -f $DIR/Dockerfile $DIR -t mucunga90/config-service:1.0.1 --rm=true
+docker image build -f $DIR/Dockerfile $DIR -t mucunga90/config-service:1.0.2 --rm=true
--- docker/start_build.sh
+++ docker/start_build.sh
@@ -3,6 +3,9 @@
 # get the absolute path of the executable
 SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH="$SELF_PATH"/$(basename -- "$0")
 
+echo -n "Docker credentials for publishing images"
+docker login
+
 echo -n "Build new images? y/n  "
 read build_images
 if [ "$build_images" == "y" ]; then
@@ -12,60 +15,48 @@ if [ "$build_images" == "y" ]; then
 	cd ./../core-service/
 	sh ./build_image.sh
 
+
   # Included service here if you want to build the docker image
 	printf "\nGenerating config-service image...\n======================================\\n"
 	cd ./../config-service/
 	sh ./build_image.sh
+	docker push mucunga90/config-service
 
 	# Included service here if you want to build the docker image
 	printf "\nGenerating discovery-service image...\n======================================\\n"
 	cd ./../discovery-service/
 	sh ./build_image.sh
+	docker push mucunga90/discovery-service
 
 	# Included service here if you want to build the docker image
 	printf "\nGenerating auth-service image...\n======================================\\n"
 	cd ./../auth-service/
 	sh ./build_image.sh
+	docker push mucunga90/auth-service
 
 	# Included service here if you want to build the docker image
 	printf "\nGenerating gateway-service image...\n======================================\\n"
 	cd ./../gateway-service/
 	sh ./build_image.sh
-
-		# Included service here if you want to build the docker image
-	printf "\nGenerating monitoring-service image...\n======================================\\n"
-	cd ./../monitoring-service/
-	sh ./build_image.sh
+	docker push mucunga90/gateway-service
 
   # Included service here if you want to build the docker image
 	printf "\nGenerating alert-service image...\n======================================\\n"
 	cd ./../alert-service/
 	sh ./build_image.sh
+	docker push mucunga90/alert-service
 
 	# Included service here if you want to build the docker image
 	printf "\nGenerating user-service image...\n======================================\\n"
 	cd ./../user-service/
 	sh ./build_image.sh
+	docker push mucunga90/user-service
 
 	# Included service here if you want to build the docker image
 	printf "\nGenerating web-service image...\n======================================\\n"
 	cd ./../web-service/
 	sh ./build_image.sh
+	docker push mucunga90/web-service
 fi
-
-if [ "$build_images" == "y" ]; then
-	echo -n "Docker credentials for publishing images"
-	docker login
-	# Included service here if you want to publish this to docker hub
-	docker push mucunga90/config-service:1.0.0
-	docker push mucunga90/discovery-service:1.0.0
-	docker push mucunga90/auth-service:1.0.0
-	docker push mucunga90/gateway-service:1.0.0
-	docker push mucunga90/alert-service:1.0.0
-	docker push mucunga90/user-service:1.0.0
-	docker push mucunga90/web-service:1.0.0
-fi
-
-cd ./../docker/
 
 
